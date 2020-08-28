1e30bf3b6304a25a79c7f27f65f1ef7fa148fc1b
muxiaochen <muxiaochen@vipkid.com.cn>
2020-08-28 17:18:06 +0800

    框架搭好

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,19 +1,35 @@
 module.exports = {
     themeConfig: {
-        logo: '/assets/img/logo.png',
+        title: '阿木木',
+        description: '阿木木的前端指南',
+        base: '/docs/',
+        logo: '/logo.png',
+        head: [
+          ['link', 
+              { rel: 'icon', href: '/logo.png' }
+              //浏览器的标签栏的网页图标，第一个'/'会遍历public文件夹的文件
+          ],  
+        ],
         // 添加导航栏
         nav: [{text: "主页", link: "/"},
-          { text: "前端",
+          { text: "前端基础",
             items: [ 
               { text: "html", link:"/web/html/"},
               { text: "css", link:"/web/css/"},
-              { text: "javascript", link:"/web/js/"},
+              { text: "javascript", link:"/javaScript/"},
             ]
           },
+          { text: "前端框架",
+            items: [ 
+              { text: "vue", link:"/frame/vue/"},
+              { text: "react", link:"/frame/react/"},
+            ]
+          },
+          { text: "TypeScript", link: "/TypeScript/"},
           { text: "node", link: "/node/"},
           { text: "http", link: "/http/"},
+          { text: "算法", link: "/arithmetic/"},
           { text: "数据库", link: "/database/"},
-          { text: "android", link: "/android/"},
           { text: "面试问题", link: "/interview/" }
         ],
         sidebar:{
