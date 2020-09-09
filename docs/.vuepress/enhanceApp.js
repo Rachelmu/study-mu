ee0404149ab4065b4ea96b904cc4f711ff7e5bf5
Rachelmu <631032057@qq.com>
2020-09-09 17:13:01 +0800

    vue role

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -12,13 +12,13 @@
 // .vuepress/enhanceApp.js
 // 全局注册 Element 组件库
 import Vue from 'vue'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import Element from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
 
 export default ({
   Vue,
   options,
   router
 }) => {
-  Vue.use(Element)
+  Vue.use()
 }
